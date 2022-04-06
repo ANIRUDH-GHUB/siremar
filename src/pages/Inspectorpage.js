3abbf7ce86d405f44a756f46307c71c8779db1bc
vijayasree <vijayasree322@gmail.com>
2022-04-06 19:14:47 +0530

    Added schools

--- src/pages/Inspectorpage.js
+++ src/pages/Inspectorpage.js
@@ -3,13 +3,24 @@
   Madhuri Mittapalli 1001856681*/
 import React, { useEffect } from "react";
 import Navbar2 from "../components/Navbar2";
-import { hostName, usersSvc,registerSvc } from "../constants/ApiEndPoints";
+import { hostName, usersSvc,registerSvc, flightSvc, schoolSvc, businessSvc } from "../constants/ApiEndPoints";
 
 function Inspectorpage() {
 const [users, setUsers] = React.useState([]);
 const [selectedUserId, setSelectedUserId] = React.useState(null);
 const [moveout, setMoveout] = React.useState();
-  
+const [title, setTitle] = React.useState();
+const [destination, setDestination] = React.useState();
+const [date, setDate] = React.useState();
+const [time, setTime] = React.useState();
+const[school, setSchool] = React.useState();
+const[state, setState] = React.useState();
+const[city, setCity] = React.useState();
+const[zip, setZip] = React.useState();
+const[bussinessDate,  setBussinessDate] = React.useState();
+const[description, setDescription] = React.useState();
+const[business,setBussiness]  = React.useState();
+
   useEffect(() => {
     const userUrl = hostName + usersSvc;
        fetch(userUrl)
@@ -19,6 +30,81 @@ const [moveout, setMoveout] = React.useState();
       }))
   },[])
 
+  const reegisterSchool = () => {
+    console.log("clicked");
+    const schoolUrl = hostName + schoolSvc;
+    const schoolObj={
+        title:school,
+        acf:{
+          city:city,
+          state:state,
+          zip: zip,
+        },
+        status: "publish",
+      }
+        fetch(schoolUrl,{
+          method: 'POST',
+          headers:{
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + localStorage.getItem('token')
+          },
+          body: JSON.stringify(schoolObj)
+        })
+        .then(response => response.json())
+       .then(data => {
+      console.log(data)
+    })
+        
+    }
+    const reegisterBusiness = () => {
+      console.log("clicked");
+      const bussinessUrl = hostName + businessSvc;
+      const businessObj={
+          title:business,
+          acf:{
+            date:bussinessDate,
+            description:description,
+          },
+          status: "publish",
+        }
+          fetch(bussinessUrl,{
+            method: 'POST',
+            headers:{
+              'Content-Type': 'application/json',
+              Authorization: 'Bearer ' + localStorage.getItem('token')
+            },
+            body: JSON.stringify(businessObj)
+          })
+          .then(response => response.json())
+         .then(data => {
+        console.log(data)
+      })
+    }
+
+  const registerFlights = () => {
+    const flightUrl = hostName + flightSvc;
+    const flight = { 
+      title: title,
+      acf:{
+      destination: destination,
+      time: time,
+      date: date,
+      },
+      status: "publish",
+      }     
+    fetch(flightUrl, {    
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + localStorage.getItem('token')
+      },
+      body: JSON.stringify(flight)
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+  }
   const handleUserChange =(e)=>{
     console.log(e.target.value);
     setSelectedUserId(e.target.value);
@@ -58,85 +144,7 @@ const [moveout, setMoveout] = React.useState();
           </strong>
         </div>
 
-        <div class="row">
-          <div class="col-sm-6">
-            <div class="row align-items-stretch mb-5">
-              <strong>
-                <p>
-                  Manage <span> Residents</span>{" "}
-                </p>
-              </strong>
-              <div class="col-md-6">
-                <div class="form-group mb-3 ">
-                  <input
-                    class="form-control"
-                    id="name"
-                    type="text"
-                    placeholder="Enter Input*"
-                    data-sb-validations="required"
-                  />
-                  <div
-                    class="invalid-feedback"
-                    data-sb-feedback="name:required"
-                  >
-                    A name is required.
-                  </div>
-                </div>
-                <div class="form-group mb-3 ">
-                  <input
-                    class="form-control"
-                    id="email"
-                    type="email"
-                    placeholder="Your Email *"
-                    data-sb-validations="required,email"
-                  />
-                  <div
-                    class="invalid-feedback"
-                    data-sb-feedback="email:required"
-                  >
-                    An email is required.
-                  </div>
-                  <div class="invalid-feedback" data-sb-feedback="email:email">
-                    Email is not valid.
-                  </div>
-                </div>
-                <div class="form-group mb-3">
-                  
-                 
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="col-sm-8">
-                  <div class="form-group form-group-textarea mb-md-0">
-                    <textarea
-                      class="form-control"
-                      id="message"
-                      cols="4"
-                      rows="5"
-                      placeholder="Your Message *"
-                      data-sb-validations="required"
-                    ></textarea>
-                    <div
-                      class="invalid-feedback"
-                      data-sb-feedback="message:required"
-                    >
-                      A message is required.
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="text-center">
-                <button
-                  class="btn btn-primary btn-xl text-uppercase disabled"
-                  id="submitButton"
-                  type="submit"
-                >
-                  Save Button
-                </button>
-              </div>
-            </div>
-          </div>
-
+     
           <div class="col-sm-6">
             <div class="row align-items-stretch mb-5">
               <strong>
@@ -150,8 +158,10 @@ const [moveout, setMoveout] = React.useState();
                     class="form-control"
                     id="name"
                     type="text"
-                    placeholder="Enter Input*"
+                    placeholder="Enter Flight Name"
                     data-sb-validations="required"
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
                   />
                   <div
                     class="invalid-feedback"
@@ -164,9 +174,11 @@ const [moveout, setMoveout] = React.useState();
                   <input
                     class="form-control"
                     id="email"
-                    type="email"
-                    placeholder="Your Email *"
+                    type="text"
+                    placeholder="Enter Flight time"
                     data-sb-validations="required,email"
+                    onChange = {(e) => setTime(e.target.value)}
+                    value={time}
                   />
                   <div
                     class="invalid-feedback"
@@ -178,42 +190,49 @@ const [moveout, setMoveout] = React.useState();
                     Email is not valid.
                   </div>
                 </div>
-                <div class="form-group mb-3">
-                  <select class="form-select" id="inputGroupSelect02">
-                    <option selected>Choose...</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                  </select>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="col-sm-8">
-                  <div class="form-group form-group-textarea mb-md-0">
-                    <textarea
-                      class="form-control"
-                      cols="4"
-                      rows="5"
-                      id="message"
-                      placeholder="Your Message *"
-                      data-sb-validations="required"
-                    ></textarea>
-                    <div
-                      class="invalid-feedback"
-                      data-sb-feedback="message:required"
-                    >
-                      A message is required.
-                    </div>
+                <div class="form-group mb-3 ">
+                  <input
+                    class="form-control"
+                    id="name"
+                    type="date"
+                    placeholder="Enter Flight Date"
+                    onChange={(e) => setDate(e.target.value)}
+                    data-sb-validations="required"
+                    value={date}
+                  />
+                  <div
+                    class="invalid-feedback"
+                    data-sb-feedback="name:required"
+                  >
+                    A name is required.
                   </div>
                 </div>
-              </div>
+                <div class="form-group mb-3 ">
+                  <input
+                    class="form-control"
+                    id="name"
+                    type="text"
+                    placeholder="Enter Flight Destination"
+                    data-sb-validations="required"
+                    onChange={(e) => setDestination(e.target.value)}
+                    value={destination}
+                  />
+                  <div
+                    class="invalid-feedback"
+                    data-sb-feedback="name:required"
+                  >
+                    A name is required.
+                  </div>
+                </div>
+
               <div class="text-center">
                 <button
-                  class="btn btn-primary btn-xl text-uppercase disabled"
+                  class="btn btn-primary btn-xl text-uppercase "
                   id="submitButton"
                   type="submit"
+                  onClick={() => registerFlights()}
                 >
-                  Save Button
+                  Save Flight
                 </button>
               </div>
             </div>
@@ -248,24 +267,6 @@ const [moveout, setMoveout] = React.useState();
                     A Date is required.
                   </div>
                 </div>
-                {/* <div class="form-group mb-3 ">
-                  <input
-                    class="form-control"
-                    id="email"
-                    type="email"
-                    placeholder="Your Email *"
-                    data-sb-validations="required,email"
-                  />
-                  <div
-                    class="invalid-feedback"
-                    data-sb-feedback="email:required"
-                  >
-                    An email is required.
-                  </div>
-                  <div class="invalid-feedback" data-sb-feedback="email:email">
-                    Email is not valid.
-                  </div>
-                </div> */}
                 <div class="form-group mb-3">
                   <select class="form-select" id="inputGroupSelect02"
                     onChange={handleUserChange}
@@ -317,7 +318,7 @@ const [moveout, setMoveout] = React.useState();
             <div class="row align-items-stretch mb-5">
               <strong>
                 <p>
-                  Inpects <span>Residents</span>{" "}
+                  Register <span>Schools</span>{" "}
                 </p>
               </strong>
               <div class="col-md-6">
@@ -326,8 +327,10 @@ const [moveout, setMoveout] = React.useState();
                     class="form-control"
                     id="name"
                     type="text"
-                    placeholder="Enter Input*"
+                    placeholder="Enter SchoolName"
                     data-sb-validations="required"
+                    onChange={(e) => setSchool(e.target.value)}
+                    value={school}
                   />
                   <div
                     class="invalid-feedback"
@@ -341,8 +344,10 @@ const [moveout, setMoveout] = React.useState();
                     class="form-control"
                     id="email"
                     type="email"
-                    placeholder="Your Email *"
+                    placeholder="Enter School State"
                     data-sb-validations="required,email"
+                    onChange={(e) => setState(e.target.value)}
+                    value={state}
                   />
                   <div
                     class="invalid-feedback"
@@ -354,46 +359,146 @@ const [moveout, setMoveout] = React.useState();
                     Email is not valid.
                   </div>
                 </div>
-                <div class="form-group mb-3">
-                  <select class="form-select" id="inputGroupSelect02">
-                    <option selected>Choose...</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                  </select>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="col-sm-8">
-                  <div class="form-group form-group-textarea mb-md-0">
-                    <textarea
-                      class="form-control"
-                      cols="4"
-                      rows="5"
-                      id="message"
-                      placeholder="Your Message *"
-                      data-sb-validations="required"
-                    ></textarea>
-                    <div
-                      class="invalid-feedback"
-                      data-sb-feedback="message:required"
-                    >
-                      A message is required.
-                    </div>
+                <div class="form-group mb-3 ">
+                  <input
+                    class="form-control"
+                    id="email"
+                    type="email"
+                    placeholder="Enter School  City"
+                    data-sb-validations="required,email"
+                    onChange={(e) => setCity(e.target.value)}
+                    value={city}
+                  />
+                  <div
+                    class="invalid-feedback"
+                    data-sb-feedback="email:required"
+                  >
+                    An email is required.
+                  </div>
+                  <div class="invalid-feedback" data-sb-feedback="email:email">
+                    Email is not valid.
                   </div>
                 </div>
+                <div class="form-group mb-3 ">
+                  <input
+                    class="form-control"
+                    id="email"
+                    type="email"
+                    placeholder="Enter School zip"
+                    data-sb-validations="required,email"
+                    onChange={(e) => setZip(e.target.value)}
+                    value={zip}
+                  />
+                  <div
+                    class="invalid-feedback"
+                    data-sb-feedback="email:required"
+                  >
+                    An email is required.
+                  </div>
+                  <div class="invalid-feedback" data-sb-feedback="email:email">
+                    Email is not valid.
+                  </div>
+                </div>
+               
               </div>
+            
               <div class="text-center">
                 <button
-                  class="btn btn-primary btn-xl text-uppercase disabled"
+                  class="btn btn-primary btn-xl text-uppercase"
                   id="submitButton"
                   type="submit"
+                  onClick={()=>reegisterSchool()}
                 >
-                  Save Button
+                  Register School
                 </button>
               </div>
             </div>
           </div>
+
+
+          <div class="col-sm-6">
+            <div class="row align-items-stretch mb-5">
+              <strong>
+                <p>
+                  Register <span>Business</span>{" "}
+                </p>
+              </strong>
+              <div class="col-md-6">
+                <div class="form-group mb-3 ">
+                  <input
+                    class="form-control"
+                    id="name"
+                    type="text"
+                    placeholder="Enter Bussiness Name"
+                    data-sb-validations="required"
+                    onChange={(e) => setBussiness(e.target.value)}
+                    value={business}
+                  />
+                  <div
+                    class="invalid-feedback"
+                    data-sb-feedback="name:required"
+                  >
+                    A name is required.
+                  </div>
+                </div>
+                <div class="form-group mb-3 ">
+                  <input
+                    class="form-control"
+                    id="email"
+                    type="date"
+                    placeholder="Enter  Date"
+                    data-sb-validations="required,email"
+                    onChange={(e) => setBussinessDate(e.target.value)}
+                    value={bussinessDate}
+                  />
+                  <div
+                    class="invalid-feedback"
+                    data-sb-feedback="email:required"
+                  >
+                    An email is required.
+                  </div>
+                  <div class="invalid-feedback" data-sb-feedback="email:email">
+                    Email is not valid.
+                  </div>
+                </div>
+                
+                <div class="form-group mb-3 ">
+                  <input
+                    class="form-control"
+                    id="email"
+                    type="text"
+                    placeholder="Enter Description"
+                    data-sb-validations="required,email"
+                    onChange={(e) => setDescription(e.target.value)}
+                    value={description}
+                  />
+                  <div
+                    class="invalid-feedback"
+                    data-sb-feedback="email:required"
+                  >
+                    An email is required.
+                  </div>
+                  <div class="invalid-feedback" data-sb-feedback="email:email">
+                    Email is not valid.
+                  </div>
+                </div>
+                   
+               
+              </div>
+            
+              <div class="text-center">
+                <button
+                  class="btn btn-primary btn-xl text-uppercase"
+                  id="submitButton"
+                  type="submit"
+                  onClick={()=>reegisterBusiness()}
+                >
+                  Register Businesss
+                </button>
+              </div>
+            </div>
+          </div>
+
           <div class="col-sm-6">
             <div class="row align-items-stretch mb-5">
               <strong>
@@ -501,6 +606,7 @@ const [moveout, setMoveout] = React.useState();
                 class="btn btn-primary btn-xl text-uppercase disabled"
                 id="submitButton"
                 type="submit"
+
               >
                 SEND{" "}
               </button>
